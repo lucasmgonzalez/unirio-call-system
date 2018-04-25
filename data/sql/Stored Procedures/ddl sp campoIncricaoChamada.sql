09d671b7d9eb33af9e6cc10d0b1e208b8c49f966
Lucas Gonzalez <lucasmgonzalez1992@gmail.com>
2018-04-25 19:33:59 -0300

    Update ddl sp campoIncricaoChamada.sql

--- data/sql/Stored Procedures/ddl sp campoIncricaoChamada.sql
+++ data/sql/Stored Procedures/ddl sp campoIncricaoChamada.sql
@@ -1,4 +1,29 @@
 --
+-- REGISTRAR VALOR DO CAMPO
+--
+
+DROP PROCEDURE IF EXISTS CadastrarInscricaoCampoChamada;
+DELIMITER //
+CREATE PROCEDURE CadastrarInscricaoCampoChamada(thisIdInscricaoChamada INT, thisIdCampoChamada INT, thisValor VARCHAR(8192))
+BEGIN
+    
+    DECLARE lCampoId INT;
+    
+    SELECT id INTO lCampoId FROM InscricaoCampoChamada 
+    WHERE idIncricao = thisIdInscricaoChamada 
+    AND idCampoChamada = thisIdCampoChamada;
+    
+	IF lCampoId IS NULL THEN
+		CALL CadastrarInscricaoCampoChamada(thisInscricaoChamada, thisIdCampoChamada, thisValor);
+    ELSE
+        CALL AlterarInscricaoCampoChamada(lCampoId, thisValor);
+	END IF;
+    
+END //
+DELIMITER ;
+
+
+--
 -- CADASTRAR CAMPO INSCRICAO CHAMADA
 --
 
@@ -17,12 +42,10 @@ DELIMITER ;
 
 DROP PROCEDURE IF EXISTS AlterarIncricaoCampoChamada;
 DELIMITER //
-CREATE PROCEDURE AlterarIncricaoCampoChamada(thisId INT, thisIdInscricaoChamada INT, thisIdCampoChamada INT, thisValor VARCHAR(8192))
+CREATE PROCEDURE AlterarIncricaoCampoChamada(thisId INT, thisValor VARCHAR(8192))
 BEGIN
     UPDATE InscricaoCampoChamada
-    SET idInscricao = thisIdInscricaoChamada,
-    idCampoChamada = thisIdCampoChamada,
-    valor = thisValor
+    SET valor = thisValor
     WHERE id = thisId;
 END //
 DELIMITER ;
