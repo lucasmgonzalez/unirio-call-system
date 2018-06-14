0ec12233dfc92b23459ccf7117a639bf40840bf5
Lucas Marques Gonzalez <lucasgonzalez@WIBSA-PC172.local>
2018-06-14 16:36:22 -0300

    Adding gulpfile, package.json, first view and initial assets

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require("gulp");
+const browserify = require("browserify");
+const babel = require("babelify");
+const sass = require("gulp-sass");
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const sourcemaps = require("gulp-sourcemaps");
+const livereload = require('gulp-livereload');
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+
+const config = {
+    sass: {
+      path: './src/main/resources/scss/',
+      entrypoint: "styles.scss",
+      export: "./src/main/webapp/styles/"
+    },
+    js: {
+      path: "./src/main/resources/js/",
+      entrypoint: "app.js",
+      export: "./src/main/webapp/scripts/"
+    }
+  };
+
+gulp.task("css", () =>
+  gulp
+    .src(config.sass.path + config.sass.entrypoint)
+    .pipe(sass())
+    .pipe(gulp.dest(config.sass.export))
+);
+
+gulp.task("js", () =>
+  browserify(config.js.path + config.js.entrypoint, { debug: true })
+    .transform(babel)
+    .bundle()
+    .on("error", err => console.error(err))
+    .pipe(source("app.js"))
+    .pipe(buffer())
+    .pipe(gulp.dest(config.js.export))
+);
+
+gulp.task('default', ['css', 'js']);
